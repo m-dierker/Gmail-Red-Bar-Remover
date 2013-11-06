53c497ce5a2b1e0834687293a0291bebf4c00566
Matthew <mvd7793@gmail.com>
2013-11-05 21:39:18 -0600

    adding everything

---
+++ gmail.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var remover = new GmailBarRemover();
+};
+
+function GmailBarRemover() {
+    this.setup();
+}
+
+GmailBarRemover.prototype.setup = function() {
+    var possible_containers = $('div:contains("having trouble renewing your storage plan")');
+    var container_to_remove = $(possible_containers[possible_containers.length - 1]).parent();
+    $(container_to_remove).hide();
+};
