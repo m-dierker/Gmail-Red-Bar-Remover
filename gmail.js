96b5cc54351e23e0d5011f6a0df47cc98923066d
Matthew Dierker <mdierker@dropbox.com>
2014-05-27 16:26:11 -0700

    Gmail is still annoying, and I don't feel like finding a better way to do it than this.

--- gmail.js
+++ gmail.js
@@ -3,11 +3,16 @@ window.onload = function() {
 };
 
 function GmailBarRemover() {
-    this.setup();
+	this.intervalTimer = setInterval(function() {
+    	this.setup();
+    }.bind(this), 1000);
 }
 
 GmailBarRemover.prototype.setup = function() {
     var possible_containers = $('div:contains("having trouble renewing your storage plan")').add('div:contains("forwarding some of your email to")');
-    var container_to_remove = $(possible_containers[possible_containers.length - 1]).parent();
-    $(container_to_remove).hide();
+    if (possible_containers.size() > 0) {
+    	var container_to_remove = $(possible_containers[possible_containers.length - 1]).parent();
+	    $(container_to_remove).hide();
+	    clearInterval(this.intervalTimer);
+    }
 };
