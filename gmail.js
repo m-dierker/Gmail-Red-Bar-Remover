e823e669f94c3fcd8a0f0ce6d6f986aa8c130e92
Matthew Dierker <mdierker@dropbox.com>
2014-05-27 16:22:29 -0700

    Gmail added another stupid red bar to my inbox. This one fades out over the course of a couple minutes, but I still think that's dumb, so I'm revisiting this extension to remove it instantly. Thanks for keeping me secure Gmail, buuuut I'm happy without a red bar I can't close. :)

--- gmail.js
+++ gmail.js
@@ -7,7 +7,7 @@ function GmailBarRemover() {
 }
 
 GmailBarRemover.prototype.setup = function() {
-    var possible_containers = $('div:contains("having trouble renewing your storage plan")');
+    var possible_containers = $('div:contains("having trouble renewing your storage plan")').add('div:contains("forwarding some of your email to")');
     var container_to_remove = $(possible_containers[possible_containers.length - 1]).parent();
     $(container_to_remove).hide();
 };
